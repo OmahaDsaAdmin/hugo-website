17864d55be5478cf4730f11520139d2a7e707574
OmahaDsaAdmin <omahadsa@gmail.com>
2026-06-15 15:52:31 -0500

    fix: fonts, errors, and csp again

--- themes/poison/assets/js/light_dark.js
+++ themes/poison/assets/js/light_dark.js
@@ -1,39 +1,55 @@
-const btn = document.querySelector(".btn-light-dark");
-const moon = document.querySelector(".moon");
-const sun = document.querySelector(".sun");
+/*
+let btn = document.querySelector(".btn-light-dark");
+let moon = document.querySelector(".moon") || { style: {} };
+let sun = document.querySelector(".sun") || { style: {} };
 
-const themeFromLS = localStorage.getItem("theme")
-const themeFromHugo = document.body.classList.contains("dark-theme") ? "dark" : null
+const themeFromLS = localStorage.getItem("theme");
+const themeFromHugo = document.body.classList.contains("dark-theme")
+  ? "dark"
+  : null;
 const currentTheme = themeFromLS ? themeFromLS : themeFromHugo;
 
+if (!btn) {
+  console.warn("uh oh! btn");
+}
+
+if (!sun) {
+  console.warn("uh oh! sun");
+}
+
+if (!moon) {
+  console.warn("uh oh! moon");
+}
+
 if (currentTheme == "dark") {
-    document.body.classList.add("dark-theme");
-    moon.style.display = 'none';
-    sun.style.display = 'block';
+  document.body.classList.add("dark-theme");
+  moon.style.display = "none";
+  sun.style.display = "block";
 } else {
-    document.body.classList.remove("dark-theme");
-    moon.style.display = 'block';
-    sun.style.display = 'none';
+  document.body.classList.remove("dark-theme");
+  moon.style.display = "block";
+  sun.style.display = "none";
 }
 
 btn.addEventListener("click", function () {
-    document.body.classList.toggle("dark-theme");
-    let hasComments = document.getElementById("remark42");
-    let theme = "light";
+  document.body.classList.toggle("dark-theme");
+  let hasComments = document.getElementById("remark42");
+  let theme = "light";
 
-    if (document.body.classList.contains("dark-theme")) {
-        theme = "dark";
-        moon.style.display = 'none';
-        sun.style.display = 'block';
-        if (hasComments) {
-            window.REMARK42.changeTheme("dark");
-        }
-    } else {
-        moon.style.display = 'block';
-        sun.style.display = 'none';
-        if (hasComments) {
-            window.REMARK42.changeTheme("light");
-        }
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+    moon.style.display = "none";
+    sun.style.display = "block";
+    if (hasComments) {
+      window.REMARK42.changeTheme("dark");
     }
-    localStorage.setItem("theme", theme);
+  } else {
+    moon.style.display = "block";
+    sun.style.display = "none";
+    if (hasComments) {
+      window.REMARK42.changeTheme("light");
+    }
+  }
+  localStorage.setItem("theme", theme);
 });
+*/
